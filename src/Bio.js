9a1086f704d5f92ded06e56833a4796368574e9e
Mike Dent <mikedent85@gmail.com>
2021-03-03 15:45:55 -0500

    landing + bio

--- src/Bio.js
+++ src/Bio.js
@@ -4,38 +4,27 @@ import dogs from './images/dogs.jpg';
 import yosemite from './images/yosemite-selfie.jpg'
 import iap from './images/iap.JPG'
 
-export class Intro extends Component {
+export class Bio extends Component {
     render() {
         return (
-            <div className='intro'>
-                <div className='intro-text'>
+            <div className='bio'>
+                <div className='bio-text'>
                     <h2 style={{fontSize: 'xxx-large'}}>about me</h2>
-                    <p>My name is Mike, and I'm a software developer in Washington, DC. After more than a decade of working in international education, I am excited to now enter the coding world. 
+                    <p>My name is Mike, and I'm a software engineer in Washington, DC. I have a decade of experience in education and curriculum development, which allowed me to hone my writing, editing, and design skills. I pride myself on projects that are creative, clean, and effective. 
                     </p>
-                    <p> I graduated from the University of Kansas with a degree in English (Creative Writing),
-                        then moved to Seoul, South Korea, to teach English and earn a master's degree in
-                        International Studies from Korea University.
+                    <p>I graduated from the University of Kansas with a bachelor’s degree in English (Creative Writing), then moved to Seoul, South Korea, to teach English and earn a master's degree in International Studies from Korea University. Upon returning to the United States, I worked at American University in Washington, DC, as a manager for graduate advisors in a program devoted to international students. My expertise in project oversight, team management, and intercultural relations were critical in guiding students to success and building relationships across the university.
                     </p>
                     <p>
-                        My journey with Thinkful has given me coding skills in <strong>Javascript</strong>, 
-                        <strong> HTML</strong>, and <strong>CSS</strong>. I love the challenge of writing these languages
-                        and the
-                        thrill that comes with completing a project. I want to work on teams that also
-                        challenge me to be even better and improve on what I've learned so far - while
-                        having a fun time, of course! Whether it is designing website templates
-                        or building mobile apps, I am ready to deliver.
+                        My journey into the coding field has sharpened my frontend skills in <strong>React</strong>, <strong>jQuery</strong>, <strong>JavaScript</strong>, <strong>HTML</strong>, & <strong>CSS</strong>, and my backend skills in <strong>Node.js</strong>, <strong>Express</strong>, & <strong>PostgreSQL</strong>. I love the challenge of writing these languages and the thrill that comes with completing a project. In the same way that I managed students’ success at university and the programs that helped them meet that success, I can see my skills expanding day by day and my projects coming to life piece by piece. I am focused and serious about my work, but I have a lot of fun while doing it.
                     </p>
-                    <p>Outside of my professional life, my personal passion is running. I have completed
-                        dozens of marathons and half-marathons around the world. Going for a long run or
-                        hike is my favorite way of clearing my mind and getting resynced with the natural world.
-                        I also have two dogs, Lainey and Sylvia, who are my hiking companions - when they are not
-                        sleeping on the couch!
+                    <p>Outside of my professional life, my biggest passion is running. I am an RRCA-certified running coach, and I have completed marathons and half-marathons around the world. Going for a long run or hike is my favorite way of clearing my mind and getting resynced with the natural world after a long day on the computer. I also have two dogs, Lainey and Sylvia, who are my hiking companions - when they are not sleeping on the couch!
+
                     </p>
                 </div>
                 <div className='bio-pics'>
-                    <img src={iap} alt='mike by world map' width='150px' style={{objectFit: 'cover'}}/>
-                    <img src={dogs} alt='mike with dogs' width='150px' style={{objectFit: 'cover'}}/>
-                    <img src={yosemite} alt='yosemite' width='150px' style={{objectFit: 'cover'}}/>
+                    <img src={iap} alt='mike by world map' style={{objectFit: 'cover'}}/>
+                    <img src={dogs} alt='mike with dogs' style={{objectFit: 'cover'}}/>
+                    <img src={yosemite} alt='yosemite' style={{objectFit: 'cover'}}/>
                 </div>
                 
             </div>
@@ -43,4 +32,4 @@ export class Intro extends Component {
     }
 }
 
-export default Intro
+export default Bio
--- src/Landing.js
+++ src/Landing.js
@@ -4,9 +4,15 @@ import './Landing.css'
 export class Landing extends Component {
     render() {
         return (
-            <div>
-                <p>I'm a full stack developer in Washington, DC.</p>
-            </div>
+            <div className='hello'>
+                    <h2 style={{fontSize: 'xxx-large'}}>hello</h2>
+                    <p>My name is <strong>Mike</strong>, and I'm a software engineer in Washington, DC. I am a full-stack developer, proficient in <strong>React</strong>, <strong>jQuery</strong>, <strong>JavaScript</strong>, <strong>HTML</strong>, & <strong>CSS</strong> on the frontend; and <strong>Node.js</strong>, <strong>Express</strong>, & <strong>PostgreSQL</strong> on the backend. 
+                    </p>
+                    <p>Please take look at my <a
+                            href="/projects" style={{color: 'inherit'}}><strong>Projects</strong></a>, or read my <a
+                            href="/bio" style={{color: 'inherit'}}><strong>Bio</strong></a> to learn a little more about me. I'd also love to <a
+                            href="/projects" style={{color: 'inherit'}}><strong>get in touch</strong></a> to talk about web development opportunities.</p>
+                    </div>
         )
     }
 }
